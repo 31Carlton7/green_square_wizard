a091bbf5ac17742803d3a579e3ea02fa9951f672
31Carlton7 <carltonaikins7@gmail.com>
2021-10-03 21:02:33 -0400

    2021-10-03T21:02:33-04:00

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-// Define Package variables
+// Define Package variable
 const moment = require('moment');
 const express = require('express');
 const jsonfile = require('jsonfile');
